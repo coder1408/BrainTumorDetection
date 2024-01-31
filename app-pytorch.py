c084837e4b6b8e9f326a0e8a4cc81eb56f107cbf
coder1408 <ishanyateenjoshi@gmail.com>
2024-01-31 14:52:58 +0530

    Initial Commit

---
+++ app-pytorch.py
@@ -0,0 +1,172 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+import torch
+from torchvision import transforms
+from PIL import Image
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2
+import numpy as np
+import glob
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
+
+class Dataset(object):
+    """An abstract class representing a Dataset.
+
+    All other datasets should subclass it. All subclasses should override
+    ``__len__``, that provides the size of the dataset, and ``__getitem__``,
+    supporting integer indexing in range from 0 to len(self) exclusive.
+    """
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __add__(self, other):
+        return ConcatDataset([self, other])
+
+class MRI(Dataset):
+    def __init__(self):
+        
+        tumor = []
+        healthy = []
+        # cv2 - It reads in BGR format by default
+        for f in glob.iglob(r"C:\Users\ishan\Desktop\Ishan\Coding\Hackathon\Dataset\Yes_Data/*.jpg"):
+            img = cv2.imread(f)
+            img = cv2.resize(img,(128,128)) # I can add this later in the boot-camp for more adventure
+            b, g, r = cv2.split(img)
+            img = cv2.merge([r,g,b])
+            img = img.reshape((img.shape[2],img.shape[0],img.shape[1])) # otherwise the shape will be (h,w,#channels)
+            tumor.append(img)
+
+        for f in glob.iglob(r"C:\Users\ishan\Desktop\Ishan\Coding\Hackathon\Dataset\No_data/*.jpg"):
+            img = cv2.imread(f)
+            img = cv2.resize(img,(128,128)) 
+            b, g, r = cv2.split(img)
+            img = cv2.merge([r,g,b])
+            img = img.reshape((img.shape[2],img.shape[0],img.shape[1]))
+            healthy.append(img)
+
+        # our images
+        tumor = np.array(tumor,dtype=np.float32)
+        healthy = np.array(healthy,dtype=np.float32)
+        
+        # our labels
+        tumor_label = np.ones(tumor.shape[0], dtype=np.float32)
+        healthy_label = np.zeros(healthy.shape[0], dtype=np.float32)
+        
+        # Concatenates
+        self.images = np.concatenate((tumor, healthy), axis=0)
+        self.labels = np.concatenate((tumor_label, healthy_label))
+        
+    def __len__(self):
+        return self.images.shape[0]
+    
+    def __getitem__(self, index):
+        
+        sample = {'image': self.images[index], 'label':self.labels[index]}
+        
+        return sample
+    
+    def normalize(self):
+        self.images = self.images/255.0
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN,self).__init__()
+        self.cnn_model = nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5),
+        nn.Tanh(),
+        nn.AvgPool2d(kernel_size=2, stride=5),
+        nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
+        nn.Tanh(),
+        nn.AvgPool2d(kernel_size=2, stride=5))
+        
+        self.fc_model = nn.Sequential(
+        nn.Linear(in_features=256, out_features=120),
+        nn.Tanh(),
+        nn.Linear(in_features=120, out_features=84),
+        nn.Tanh(),
+        nn.Linear(in_features=84, out_features=1))
+        
+    def forward(self, x):
+        x = self.cnn_model(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_model(x)
+        x = torch.sigmoid(x)
+        
+        return x
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'jpg'}
+path = r'Project\FinalModel2.pth'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = CNN().to(device)
+
+# Load yotrained model
+model_instance = torch.load(path)
+
+# Set the model in evaluation mode
+model.eval()
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+def preprocess_image(image_path):
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+    ])
+    image = Image.open(image_path).convert('RGB')
+    return transform(image).unsqueeze(0)
+
+def create_upload_folder():
+    upload_folder = app.config['UPLOAD_FOLDER']
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+
+create_upload_folder()
+
+@app.route('/')
+def index():
+    return render_template('index-pytorch.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    create_upload_folder()  # Ensure the upload folder exists
+
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Make predictions
+        input_tensor = preprocess_image(file_path)
+        
+        # Use the loaded model for prediction
+        with torch.no_grad():
+            output = model(input_tensor)  # Output is a tensor
+            prediction = "Tumor Detected" if output.item() > 0.5 else "No Tumor"
+            
+        return render_template('index-pytorch.html', prediction=prediction, filename=filename)
+
+    return redirect(request.url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app-tensorflow.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+from PIL import Image
+import numpy as np
+import cv2
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = r'Project\static\uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'jpg'}
+
+# Load the trained model
+model_path = r'Project\brain-tumor1.h5'  # Replace with the actual path to your trained model
+model = load_model(model_path)
+
+# Preprocess image for prediction
+def preprocess_image(image_path):
+    img = image.load_img(image_path, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    return render_template('index-tensorflow.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        print(file_path)
+
+        # Make predictions
+        input_tensor = preprocess_image(file_path)
+
+        # Use the loaded model for prediction
+        with tf.device('/cpu:0'):  # Use CPU to avoid GPU memory issues
+            prediction = model.predict(input_tensor)
+
+        class_labels = ['glioma_tumor', 'meningioma_tumor', 'no_tumor', 'pituitary_tumor']
+        predicted_class = class_labels[np.argmax(prediction)]
+
+        return render_template('index-tensorflow.html', prediction=predicted_class, image_path=file_path)
+
+    return redirect(request.url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
